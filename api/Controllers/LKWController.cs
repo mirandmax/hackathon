457cce6169cc08e45b3bba63dec14ed3714d7b1d
Max Mirandola <max.mirandola@icloud.com>
2023-11-11 00:00:45 +0100

    API

--- api/Controllers/LKWController.cs
+++ api/Controllers/LKWController.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using MySql.Data;
+
 
 using api.Models;
+using System.Text.Json;
+using MySql.Data.MySqlClient;
 
 namespace api.Controllers
 {
@@ -8,11 +12,40 @@ namespace api.Controllers
     [ApiController]
     public class LKWController : ControllerBase
     {
+        private class LeaderboardRecord
+        {
+            public string Name { get; set; } = string.Empty;
+
+            public int Credits { get; set; }
+        }
         // GET api/LKW
         [HttpGet]
-        public ActionResult<string> Get()
+        public ActionResult<string> GetLeaderboard()
         {
-            return "LKW";
+            var result = new List<LeaderboardRecord>();
+            using(var connection = new MySqlConnection("Server=localhost;Database=truckit;Uid=root;Pwd=;"))
+            {
+                connection.Open();
+
+                using(var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT uname, ucredits FROM user ORDER BY ucredits DESC LIMIT 25";
+
+                    using(var reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            result.Add(new LeaderboardRecord
+                            {
+                                Name = reader.GetString(0),
+                                Credits = reader.GetInt32(1)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return JsonSerializer.Serialize(result);
         }
 
         // POST api/LKW
@@ -20,6 +53,34 @@ namespace api.Controllers
         public ActionResult<CreationReward> Post(LKWCreationDto lkw)
         {
             CreationReward reward = new CreationReward(true, 10);
+
+            using(var connection = new MySqlConnection("Server=localhost;Database=truckit;Uid=root;Pwd=;"))
+            {
+                connection.Open();
+
+                using(var command = connection.CreateCommand())
+                {
+                    command.CommandText = "SELECT FROM ";
+                    command.Parameters.AddWithValue("@name", lkw.CompanyName);
+                    command.Parameters.AddWithValue("@credits", reward.Reward);
+
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            using(var connection = new MySqlConnection("Server=localhost;Database=truckit;Uid=root;Pwd=;"))
+            {
+                connection.Open();
+
+                using(var command = connection.CreateCommand())
+                {
+                    command.CommandText = "INSERT INTO user (name, credits) VALUES (@name, @credits)";
+                    command.Parameters.AddWithValue("@name", lkw.CompanyName);
+                    command.Parameters.AddWithValue("@credits", reward.Reward);
+
+                    command.ExecuteNonQuery();
+                }
+            }
 
             return Ok(reward);
         }
---
+++ api/Models/User.cs
@@ -0,0 +1,11 @@
+namespace api.Models
+{
+    public class User
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Password { get; set; } = string.Empty;
+
+        public string Mail { get; set; } = string.Empty;
+    }
+}
